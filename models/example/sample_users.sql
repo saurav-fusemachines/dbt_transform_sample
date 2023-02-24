bd49305511119ec186850c4b0fd43b0fff1a8c34
Saurav Karki <saurav.karki@fusemachines.com>
2023-02-24 22:18:00 +0545

    updated sql

---
+++ models/example/sample_users.sql
@@ -0,0 +1,77 @@
+{{ config (
+    materialized="table"
+)}}
+
+  create  table "DataWareHouseX".public."sample_users__dbt_tmp"
+  as (
+    
+with __dbt__cte__sample_users_ab1 as (
+
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+-- depends_on: "DataWareHouseX".public._airbyte_raw_sample_users
+select
+    jsonb_extract_path_text(_airbyte_data, '_id') as _id,
+    jsonb_extract_path_text(_airbyte_data, 'age') as age,
+    jsonb_extract_path_text(_airbyte_data, 'name') as "name",
+    jsonb_extract_path_text(_airbyte_data, 'email') as email,
+    jsonb_extract_path_text(_airbyte_data, 'location') as "location",
+    
+        jsonb_extract_path(table_alias._airbyte_data, 'contact_number')
+     as contact_number,
+    jsonb_extract_path_text(_airbyte_data, 'eid_aibyte_transform') as eid_aibyte_transform,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at
+from "DataWareHouseX".public._airbyte_raw_sample_users as table_alias
+-- sample_users
+where 1 = 1
+),  __dbt__cte__sample_users_ab2 as (
+
+-- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
+-- depends_on: __dbt__cte__sample_users_ab1
+select
+    cast(_id as text) as _id,
+    cast(age as 
+    float
+) as age,
+    cast("name" as text) as "name",
+    cast(email as text) as email,
+    cast("location" as text) as "location",
+    cast(contact_number as 
+    jsonb
+) as contact_number,
+    cast(eid_aibyte_transform as text) as eid_aibyte_transform,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at
+from __dbt__cte__sample_users_ab1
+-- sample_users
+where 1 = 1
+),  __dbt__cte__sample_users_ab3 as (
+
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: __dbt__cte__sample_users_ab2
+select
+    md5(cast(coalesce(cast(_id as text), '') || '-' || coalesce(cast(age as text), '') || '-' || coalesce(cast("name" as text), '') || '-' || coalesce(cast(email as text), '') || '-' || coalesce(cast("location" as text), '') || '-' || coalesce(cast(contact_number as text), '') || '-' || coalesce(cast(eid_aibyte_transform as text), '') as text)) as _airbyte_sample_users_hashid,
+    tmp.*
+from __dbt__cte__sample_users_ab2 tmp
+-- sample_users
+where 1 = 1
+)-- Final base SQL model
+-- depends_on: __dbt__cte__sample_users_ab3
+select
+    _id,
+    age,
+    "name",
+    email,
+    "location",
+    contact_number,
+    eid_aibyte_transform,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at,
+    _airbyte_sample_users_hashid
+from __dbt__cte__sample_users_ab3
+-- sample_users from "DataWareHouseX".public._airbyte_raw_sample_users
+where 1 = 1
+  );
---
+++ models/example/sample_users_contact_number.sql
@@ -0,0 +1,62 @@
+
+
+  create  table "DataWareHouseX".public."sample_users_contact_number__dbt_tmp"
+  as (
+    
+with __dbt__cte__sample_users_contact_number_ab1 as (
+
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+-- depends_on: "DataWareHouseX".public."sample_users"
+select
+    _airbyte_sample_users_hashid,
+    jsonb_extract_path_text(contact_number, 'home') as home,
+    jsonb_extract_path_text(contact_number, 'office') as office,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at
+from "DataWareHouseX".public."sample_users" as table_alias
+-- contact_number at sample_users/contact_number
+where 1 = 1
+and contact_number is not null
+),  __dbt__cte__sample_users_contact_number_ab2 as (
+
+-- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
+-- depends_on: __dbt__cte__sample_users_contact_number_ab1
+select
+    _airbyte_sample_users_hashid,
+    cast(home as 
+    float
+) as home,
+    cast(office as 
+    float
+) as office,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at
+from __dbt__cte__sample_users_contact_number_ab1
+-- contact_number at sample_users/contact_number
+where 1 = 1
+),  __dbt__cte__sample_users_contact_number_ab3 as (
+
+-- SQL model to build a hash column based on the values of this record
+-- depends_on: __dbt__cte__sample_users_contact_number_ab2
+select
+    md5(cast(coalesce(cast(_airbyte_sample_users_hashid as text), '') || '-' || coalesce(cast(home as text), '') || '-' || coalesce(cast(office as text), '') as text)) as _airbyte_contact_number_hashid,
+    tmp.*
+from __dbt__cte__sample_users_contact_number_ab2 tmp
+-- contact_number at sample_users/contact_number
+where 1 = 1
+)-- Final base SQL model
+-- depends_on: __dbt__cte__sample_users_contact_number_ab3
+select
+    _airbyte_sample_users_hashid,
+    home,
+    office,
+    _airbyte_ab_id,
+    _airbyte_emitted_at,
+    now() as _airbyte_normalized_at,
+    _airbyte_contact_number_hashid
+from __dbt__cte__sample_users_contact_number_ab3
+-- contact_number at sample_users/contact_number from "DataWareHouseX".public."sample_users"
+where 1 = 1
+  );
