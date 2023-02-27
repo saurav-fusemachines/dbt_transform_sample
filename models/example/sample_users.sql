34575af2e5ef98ae4d9fa49af29664bcf1b10be6
Saurav Karki <saurav.karki@fusemachines.com>
2023-02-27 14:11:05 +0545

    updated query

--- models/example/sample_users.sql
+++ models/example/sample_users.sql
@@ -1,15 +1,4 @@
-select
-    _id,
-    age,
-    Upper("name"),
-    email,
-    "location",
-    contact_number,
-    eid_aibyte_transform,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
-    now() as _airbyte_normalized_at,
-    _airbyte_sample_users_hashid
+select 
+upper("name") as Name,email,"location" ,contact_number  
 from 
-sample_users from "DataWareHouseX".public.sample_users
-  );
+sample_users;
